df154d0b877b24e14118de17079dc9bfd160aacf
Umesh <uk.mahajan11@gmailcom>
2022-08-04 19:38:46 +0530

    Views add as per the role of login user

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,9 +4,15 @@ class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def new
+    # if current_user.role == 'employee'
+    #   redirect_to user_path
+    # elsif current_user.role == 'hr'
+    #   redirect_to users_path
+    # else
+    #   redirect_to users_path
+    # end
+  end
 
   # POST /resource/sign_in
   # def create
