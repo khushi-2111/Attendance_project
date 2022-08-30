226147558f0bd9cbcffaca4017b8115f00a46401
khushi sharma <khushisharam2332@gmail.com>
2022-08-30 13:02:24 +0530

    created different controllers for different routes

--- app/models/workday.rb
+++ app/models/workday.rb
@@ -2,9 +2,5 @@ class Workday < ApplicationRecord
   belongs_to :user
   has_many :worklogs
 
-<<<<<<< Updated upstream
   enum status: [:Leave, :Half_Day, :Early_Going, :Full_Day]
-=======
-  enum status: [:leave, :half_day, :early_going, :full_day]
->>>>>>> Stashed changes
 end
